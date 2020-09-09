b484eafd2eb38061b53f1ba8bb0486e4b496a826
Limin Deng <limin.deng@anu.edu.au>
2020-09-09 22:45:24 +1000

    add paper

--- code/submission1.py
+++ code/submission1.py
@@ -1,5 +1,6 @@
 
-# This agent random chooses a non-empty column.
+# version 1: # This agent random chooses a non-empty column.
+import random
+
 def my_agent(obs, config):
-    import random
     return random.choice([c for c in range(config.columns) if obs.board[c] == 0])
